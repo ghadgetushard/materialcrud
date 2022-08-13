097f78c3b52f1abab60e8f4a2c53b6cd467c999f
Tushar Ghadge <ghadgetushard@gmail.com>
2022-08-13 17:10:10 +0530

    changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'first_material_crud';
+export class AppComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  title = 'first_material_crud';
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment'];
+
+  dataSource !: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  constructor(private dialog: MatDialog, private api: ApiService) { }
+
+  ngOnInit() {
+    this.getAllProducts();
+  }
 
   openDialog() {
     this.dialog.open(DialogComponent, {
       width: '30%'
     });
   }
+
+  getAllProducts() {
+    this.api.getProduct()
+      .subscribe({
+        next: (res) => {
+          this.dataSource = new MatTableDataSource(res);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        },
+        error: (err) => {
+          console.log('Error While ', err);
+        }
+      })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if(this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 }
